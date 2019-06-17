14f689c0b76222eadc8fb758df95302f7024e299
Moses Devadason <Moses_Devadason@Dell.com>
2019-06-17 09:43:24 -0700

    XOR linked list contd..

--- practice_prbs/prb6/solution.cpp
+++ practice_prbs/prb6/solution.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
 
 /**
  * @brief Node structure to be inserted into the list.
@@ -20,6 +21,8 @@ class Xor_linked_list
     private:
         /** First node of the list. */
         list_node_t *head;
+        /** Last node of the list. */
+        list_node_t *tail;
         /** Size of the list. */
         std::size_t size;
         /**
@@ -85,24 +88,63 @@ std::size_t Xor_linked_list::list_size(void)
 
 list_node_t * Xor_linked_list::list_get(std::size_t index)
 {
-    if(nullptr == this->head) {
+    if (nullptr == this->head) {
         std::printf("List is empty!\n");
         return nullptr;
     }
  
     list_node_t *node = this->head, *p = nullptr, *tmp = nullptr;
     do {
-        if(0 == index) {
-            return node;
-        }
+        if (0 == index) return node;
         /* Since each node's xor_ptr contains the XOR of the prevous and next
          * node's addresses, XOR of the prevous node's address and the current
          * xor_ptr would give the address of the next node. A ^ (A ^ B) = B. */
         tmp = node;
-        node = p ^ node->xor_ptr;
+        node = (list_node_t *) ((uintptr_t)p ^ (uintptr_t)node->xor_ptr);
+        std::printf("Node traversed : %d. Address : %p\n", node->data, node);
         p = tmp;
         --index;
     }while(node != nullptr);
 
     return nullptr;
+}
+
+void Xor_linked_list::list_append(int data)
+{
+    list_node_t *node = new list_node_t;
+    list_node_t *p = nullptr;
+
+    node->data = data;
+    std::printf("Inserted %d. Address : %p\n", data, node);
+
+    // List is empty.
+    if (nullptr == this->head) {
+        this->head = node;
+        this->tail = node;
+        node->xor_ptr = nullptr;
+        return;
+    }
+    
+    /* Since tail is the last node, we XOR nullptr with the xor_pointer to get
+     * previous node. */
+    p = (list_node_t *) ((uintptr_t)(this->tail->xor_ptr) ^ (uintptr_t)nullptr);
+    std::printf("Inserting %d. Previous node address : %p\n", node->data, p);
+    // xor_ptr for the new node. Update tail pointer.
+    node->xor_ptr = (list_node_t *) ((uintptr_t)p ^ (uintptr_t)nullptr);
+    std::printf("Inserted %d. XOR ptr : %p\n", node->data, node->xor_ptr);
+    tail = node;
+}
+
+int main(int argc, char *argv[])
+{
+    Xor_linked_list my_list;
+
+    my_list.list_append(10);
+    std::printf("Node 0 : %d\n", my_list.list_get(0)->data);
+    my_list.list_append(20);
+    std::printf("Node 1 : %d\n", my_list.list_get(1)->data);
+    my_list.list_append(30);
+    std::printf("Node 2 : %d\n", my_list.list_get(2)->data);
+
+    return 0;
 }
