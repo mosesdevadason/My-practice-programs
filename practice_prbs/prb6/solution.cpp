c16fca5bbb0ddf621e01fe570fbb2f5c86ec3985
Moses Devadason <Moses_Devadason@Dell.com>
2019-06-18 02:09:00 -0700

    Finished XOR linked list

--- practice_prbs/prb6/solution.cpp
+++ practice_prbs/prb6/solution.cpp
@@ -70,6 +70,10 @@ class Xor_linked_list
          */
         void list_delete(size_t index);
         /**
+         * @brief Destroys the complete list.
+         */
+        void list_destroy(void);
+        /**
          * @brief Returns the current size of the list.
          * @return Size of the list.
          */
@@ -84,7 +88,7 @@ Xor_linked_list::Xor_linked_list(void)
 
 Xor_linked_list::~Xor_linked_list(void)
 {
-
+    this->list_destroy();
 }
 
 void Xor_linked_list::list_size_inc(void)
@@ -115,7 +119,7 @@ list_node_t * Xor_linked_list::_list_get(size_t index, list_node_t **p,
     do {
         if (0 == index) {
             // Get the next node to return.
-            *n = (list_node_t *) ((uintptr_t)node ^ (uintptr_t)*p);
+            *n = (list_node_t *) ((uintptr_t)node->xor_ptr ^ (uintptr_t)*p);
             return node;
         }
         /* Since each node's xor_ptr contains the XOR of the prevous and next
@@ -123,7 +127,7 @@ list_node_t * Xor_linked_list::_list_get(size_t index, list_node_t **p,
          * xor_ptr would give the address of the next node. A ^ (A ^ B) = B. */
         tmp = node;
         node = (list_node_t *) ((uintptr_t)*p ^ (uintptr_t)node->xor_ptr);
-        std::printf("Node traversed : %d. Address : %p\n", node->data, node);
+        //std::printf("Node traversed : %d. Address : %p\n", node->data, node);
         *p = tmp;
         --index;
     }while(node != nullptr);
@@ -145,18 +149,33 @@ void Xor_linked_list::list_delete(size_t index)
     list_node_t *pp = nullptr, *nn = nullptr;
 
     node = this->_list_get(index, &p, &n);
-    /* pp is the node previous to the p. This can be obtained by XORing
-     * address of current node with xor ptr of p. */
-    pp = (list_node_t *) ((uintptr_t)p->xor_ptr ^ (uintptr_t)node);
-    /* nn is the node next to n. This can be obtaned by XORing the address of
-     * current node and xor_ptr of n. */
-    nn = (list_node_t *) ((uintptr_t)node ^ (uintptr_t)n->xor_ptr);
-    // Delete node and re-calculate xor_ptrs of p and n.
+    if (p != nullptr) {
+        /* pp is the node previous to p. This can be obtained by XORing address
+         * of current node with xor ptr of p. */
+        pp = (list_node_t *) ((uintptr_t)p->xor_ptr ^ (uintptr_t)node);
+        // Re-calculate XOR_ptr of p.
+        p->xor_ptr = (list_node_t *) ((uintptr_t)pp ^ (uintptr_t)n);
+    }
+    if (n != nullptr) {
+        /* nn is the node next to n. This can be obtaned by XORing the address
+         * of current node and xor_ptr of n. */
+        nn = (list_node_t *) ((uintptr_t)node ^ (uintptr_t)n->xor_ptr);
+        // Re-calculate XOR_ptr of n.
+        n->xor_ptr = (list_node_t *) ((uintptr_t)p ^ (uintptr_t)nn);
+    }
+    if (this->head == node) this->head = n;
+    if (this->tail == node) this->tail = p;
+
     delete node;
-    p->xor_ptr = (list_node_t *) ((uintptr_t)pp ^ (uintptr_t)n);
-    n->xor_ptr = (list_node_t *) ((uintptr_t)p ^ (uintptr_t)nn);
 
     this->list_size_dec();
+}
+
+void Xor_linked_list::list_destroy(void)
+{
+    while(this->list_size() != 0) {
+        this->list_delete(0);
+    }
 }
 
 void Xor_linked_list::list_append(int data)
@@ -171,6 +190,7 @@ void Xor_linked_list::list_append(int data)
         this->head = node;
         this->tail = node;
         node->xor_ptr = nullptr;
+        this->list_size_inc();
         return;
     }
     
@@ -196,6 +216,9 @@ int main(int argc, char *argv[])
     my_list.list_append(40);
     my_list.list_append(50);
     my_list.list_append(60);
+    my_list.list_append(70);
+    my_list.list_append(80);
+    my_list.list_append(90);
 
     for (size_t ix = 0 ; ix < my_list.list_size() ; ++ix) {
         std::printf("Node %lu : %d\n", ix, my_list.list_get(ix)->data);
@@ -203,7 +226,26 @@ int main(int argc, char *argv[])
 
     std::printf("Deleted node at 2nd position!\n");
     my_list.list_delete(2);
+    for (size_t ix = 0 ; ix < my_list.list_size() ; ++ix) {
+        std::printf("Node %lu : %d\n", ix, my_list.list_get(ix)->data);
+    }
+    
+    std::printf("Deleted node at 0th position!\n");
+    my_list.list_delete(0);
 
+    for (size_t ix = 0 ; ix < my_list.list_size() ; ++ix) {
+        std::printf("Node %lu : %d\n", ix, my_list.list_get(ix)->data);
+    }
+
+    std::printf("Deleted node at last position!\n");
+    my_list.list_delete(my_list.list_size() - 1);
+
+    for (size_t ix = 0 ; ix < my_list.list_size() ; ++ix) {
+        std::printf("Node %lu : %d\n", ix, my_list.list_get(ix)->data);
+    }
+    
+    std::printf("Inserted node with data 200\n");
+    my_list.list_append(200);
     for (size_t ix = 0 ; ix < my_list.list_size() ; ++ix) {
         std::printf("Node %lu : %d\n", ix, my_list.list_get(ix)->data);
     }
