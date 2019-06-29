5d2e4cc7c3226ccdb88c0898fa23989ebe4c3cbe
Moses Devadason <mosesdevadason@gmail.com>
2019-06-29 17:19:49 +0530

    Completed serialize deserialize with leetcode

--- practice_prbs/prb3/solution.cpp
+++ practice_prbs/prb3/solution.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <string>
-#include <vector>
 #include <queue>
+#include <sstream>
 
 struct TreeNode
 {
@@ -11,104 +11,62 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
-size_t tree_height(TreeNode *root)
-{
-    if (nullptr == root) return 0;
-
-    return std::max((1 + tree_height(root->left)),
-            (1 + tree_height(root->right)));
-}
-
-std::string serialize_level(TreeNode *root, size_t curr_depth, size_t req_depth)
-{
-    std::string retval = "";
-
-    if (nullptr == root) return "n,";
-
-    if (curr_depth == req_depth) return std::to_string(root->val) + ",";
-
-    if ((curr_depth + 1) <= req_depth) {
-        retval = retval +
-            serialize_level(root->left, (curr_depth + 1), req_depth);
-        retval = retval +
-            serialize_level(root->right, (curr_depth + 1), req_depth);
-    }
-
-    return retval;
-}
-
 std::string serialize_level_order(TreeNode *root)
 {
-    std::string retval = "";
+    if (nullptr == root) {return "";}
 
-    if (nullptr == root) return "";
+    std::string ser_str = "";
+    std::queue<TreeNode *> q;
 
-    size_t height = tree_height(root);
-    for (size_t ix = 0 ; ix < height ; ++ix) {
-        retval = retval + 
-            serialize_level(root, 0 /* curr_depth */, ix /* req_depth*/);
-    }
-
-    if (',' == retval.at(retval.size() - 1)) retval.erase(retval.size() - 1);
-    return retval;
-}
-
-std::vector<std::string> split_serialized_string (std::string& serialized_str)
-{
-    std::vector<std::string> result;
-    std::string local_serialized_str = {serialized_str}, delimiter = ",";
-
-    size_t pos = 0;
-    std::string token;
-    while ((pos = local_serialized_str.find(delimiter)) != std::string::npos) {
-        token = local_serialized_str.substr(0, pos);
-        result.push_back(token);
-        local_serialized_str.erase(0, pos + delimiter.length());
-    }
-    // Add the final token
-    result.push_back(local_serialized_str);
-
-    return result;
-}
-
-TreeNode * deserialize_level_order(std::string serialized_string)
-{
-    if (serialized_string.length() == 0) return nullptr;
-
-    std::vector<std::string> items;
-    items = split_serialized_string(serialized_string);
-
-    /* 1. Insert first node.
-     * 2. Push node into Queue
-     * 3. For every item in front of the queue,
-     *     4. Left = 2*i + 1 and Right = 2*i + 2.
-     *     6. If left or right position is out of bounds insert whatever is
-     *        inside bounds alone.
-     *     5. Push the left and right (whatever is in-bounds) children into the
-     *        queue.
-     *     6. Delete the processed node (in front) from the queue.
-     *     7. Repeat from step 4 for item in front of queue until queue is
-     *        empty.
-     */
-    std::queue<std::pair<TreeNode *, size_t>> q;
-    TreeNode *root = new TreeNode(std::stoi(items[0]));
-    q.push(std::pair<TreeNode *, size_t> (root, 0));
-
+    q.push(root);
     while (!q.empty()) {
-        int ppos = q.front().second;
-        int lpos = (2 * ppos) + 1;
-        int rpos = (2 * ppos) + 2;
-        if (lpos < items.size() && items[lpos] != "n") {
-            q.front().first->left = new TreeNode(std::stoi(items[lpos]));
-            q.push(std::pair<TreeNode *, size_t> (q.front().first->left, lpos));
-        }
-        if (rpos < items.size() && items[rpos] != "n") {
-            q.front().first->right = new TreeNode(std::stoi(items[rpos]));
-            q.push(std::pair<TreeNode *, size_t> (q.front().first->right, rpos));
+        if (nullptr == q.front()) {
+            /* using ser_str = ser_str + "n " causes memory exceeded error in
+             * leetcode */
+            ser_str += "n ";
+        } else {
+            ser_str += std::to_string(q.front()->val) + " ";
+            q.push(q.front()->left);
+            q.push(q.front()->right);
         }
         q.pop();
     }
-    
+
+    // Remove trailing whitespace
+    while (ser_str[ser_str.length() - 1] == ' ') {
+        ser_str.erase(ser_str.length() - 1);
+    }
+
+    return ser_str;
+}
+
+TreeNode * deserialize_level_order(std::string& serialized_str)
+{
+    if (0 == serialized_str.length()) {return nullptr;}
+
+    std::istringstream ser_stream(serialized_str);
+    std::string token = "";
+
+    ser_stream >> token;
+    TreeNode *root = new TreeNode(std::stoi(token));
+    std::queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty()) {
+        // Left child.
+        ser_stream >> token;
+        if (token != "n") {
+            q.front()->left = new TreeNode(std::stoi(token));
+            q.push(q.front()->left);
+        }
+        // Right child.
+        ser_stream >> token;
+        if (token != "n") {
+            q.front()->right = new TreeNode(std::stoi(token));
+            q.push(q.front()->right);
+        }
+        q.pop();
+    }
+
     return root;
 }
 
@@ -123,8 +81,7 @@ void print_inorder(TreeNode *root)
 
 int main(int argc, char *argv[])
 {
-    // [1,2,3,1,3,2,4] - PASS
-    // [5,2,3,null,null,2,4,3,1] - FAIL
+    // [5,2,3,null,null,2,4,3,1]
     TreeNode *n1 = new TreeNode(5);
 
     TreeNode *n2 = n1->left = new TreeNode(2);
@@ -135,12 +92,11 @@ int main(int argc, char *argv[])
     TreeNode *n7 = n4->right = new TreeNode(1);
 
 
-    std::printf("Height of the tree : %lu\n", tree_height(n1));
-    std::string serialized_string = serialize_level_order(n1);
+    std::string serialized_str = serialize_level_order(n1);
     std::printf("Level order serialized tree : %s\n",
-            serialized_string.c_str());
-    TreeNode *result = deserialize_level_order(serialized_string);
-    print_inorder(result);
+            serialized_str.c_str());
+    TreeNode *root = deserialize_level_order(serialized_str);
+    print_inorder(root);
 
     return 0;
 }
