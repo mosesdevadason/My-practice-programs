4c0b1054ed90c0534273421ac673dce5f074dde8
Moses Devadason <mosesdevadason@gmail.com>
2019-07-30 22:50:13 +0530

    prb11 compilation sucess

--- practice_prbs/prb11/solution.cpp
+++ practice_prbs/prb11/solution.cpp
@@ -6,6 +6,7 @@
 struct trie_node_t
 {
     char c = '\0';
+    bool end_of_word = false;
     std::unordered_map<char, trie_node_t *> ptrs;
 
     trie_node_t(char x)
@@ -14,28 +15,24 @@ struct trie_node_t
     }
 };
 
-void trie_insert_word(trie_node_t *root, std::string& s)
+void get_suggestions__(trie_node_t *root, std::string& s,
+        std::vector<std::string>& suggestions)
 {
-    tree_node_t *p = root;
+    if (root->end_of_word) suggestions.push_back(s);
 
-    for (size_t ix = 0 ; ix < s.size() ; ++ix) {
-        auto it = p->ptrs.find(s[ix]);
-        if (it == p->ptrs.end()) { // Char not present in pointer set.
-            // Create new node and traverse to it. Move on with next char.
-            tree_node_t *n = new tree_node_t(s[ix]);
-            root->ptrs.emplace(s[ix], n);
-            p = n;
-        } else { // Char present in pointer set.
-            // Traverse to next node and move on to next char.
-            p = it->second;
-        }
+    if (root->ptrs.empty()) return;
+
+    for (auto it = root->ptrs.begin() ; it != root->ptrs.end() ; ++it) {
+        s = s + it->second->c;
+        std::printf("s = %s\n", s.c_str());
+        get_suggestions__(it->second, s, suggestions);
     }
 }
 
 std::vector<std::string> get_suggestions(trie_node_t *root, std::string& s)
 {
-    tree_node_t *p = root;
-    std::vector suggestions;
+    trie_node_t *p = root;
+    std::vector<std::string> suggestions;
 
     for (size_t ix = 0; ix < s.size() ; ++ix) {
         auto it = p->ptrs.find(s[ix]);
@@ -45,20 +42,59 @@ std::vector<std::string> get_suggestions(trie_node_t *root, std::string& s)
 
     // Completed walking the prefix. Every possibility from here must be added
     // to suggestions list.
+    get_suggestions__(p, s, suggestions);
+    
+    return suggestions;
 }
+
+void trie_insert_word(trie_node_t *root, std::string& s)
+{
+    trie_node_t *p = root;
+
+    for (size_t ix = 0 ; ix < s.size() ; ++ix) {
+        auto it = p->ptrs.find(s[ix]);
+        if (it == p->ptrs.end()) { // Char not present in pointer set.
+            // Create new node and traverse to it. Move on with next char.
+            trie_node_t *n = new trie_node_t(s[ix]);
+            p->ptrs.emplace(s[ix], n);
+            p = n;
+        } else { // Char present in pointer set.
+            // Traverse to next node and move on to next char.
+            p = it->second;
+        }
+    }
+    // Mark the last node as end of word.
+    p->end_of_word = true;
+}
+
 
 trie_node_t * trie_construct(std::vector<std::string>& dict)
 {
     trie_node_t *root = new trie_node_t('\0');
 
-    for (auto it = dict.begin ; it->dict.end() ; ++it) {
+    for (auto it = dict.begin() ; it != dict.end() ; ++it) {
         trie_insert_word(root, *it);
     }
 
     return root;
 }
 
+std::vector<std::string> get_suggestions_for_dict_and_query(
+        std::vector<std::string>& dict, std::string& query
+        )
+{
+    trie_node_t *root = trie_construct(dict);
+
+    return get_suggestions(root, query);
+}
+
 int main(int argc, char *argv[])
 {
+    std::vector<std::string> dict = {"dog", "deal", "deer"};
+    std::string query = "de";
+
+    std::vector<std::string> suggestions =
+        get_suggestions_for_dict_and_query(dict, query);
+
     return 0;
 }
